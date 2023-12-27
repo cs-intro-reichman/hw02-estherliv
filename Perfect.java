5f20e918350882e97905a16f8ce02e2af8f9d2ef
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-27 21:39:17 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,28 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		
+		int num = Integer.parseInt(args[0]);
+        System.out.print(num + " is");
+
+        int sum = 1;
+        int count = 0;
+
+         for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                if (count > 0) {
+                    System.out.print(" +");
+                }
+                System.out.print(" " + i);
+                sum += i;
+                count++;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println(" a perfect number since " + num + " = " + sum);
+        } else {
+            System.out.println(" not a perfect number.");
+        }
+    }
 }
