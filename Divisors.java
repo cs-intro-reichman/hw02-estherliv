b02037e12790c1d6c199199e85b9c2cbe1b956a5
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-27 20:26:31 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -5,7 +5,7 @@ public class Divisors {
 	public static void main (String[] args) {
 		int num = Integer.parseInt(args[0]);
 		for (int i = 1; i <= num; i++) {
-            if (number % i == 0) {
+            if (num % i == 0) {
                 System.out.println(i);
             }
         }
