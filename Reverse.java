e547fd87090e9be03ad7b11726909ffa6bca62b9
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-27 20:37:07 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,15 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String str1 = args[0];
+		String str2 = "";
+		int n = str1.length();
+
+		for (int i = 0; i < n; i++) {
+			str2 = str2 + str1.charAt(n - i - 1);
+		}
+
+		System.out.println(str2);
+		System.out.println("The middle character is " + str1.charAt((n - 1) / 2));
 	}
 }
