73600f1c1b8b0fcad716c4dd9ec959fad1f4c5e7
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-31 11:19:57 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,15 +1,13 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
 public class DamkaBoard {
 	public static void main(String[] args) {
 	int n = Integer.parseInt(args[0]);
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2 * n; j++) {
+            for (int j = 0; j < n; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.print("* ");
+                    System.out.print("*  ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("   ");
                 }
             }
             System.out.println();
