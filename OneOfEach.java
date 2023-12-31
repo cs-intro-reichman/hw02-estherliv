e04627363ef45b52be46f1c1e137d607c6bffe4e
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-31 10:13:03 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,20 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		boolean boy = false;
+        boolean girl = false;
+        int total = 0;
+
+        while (!boy || !girl) {
+            if (Math.random() < 0.5) {
+                System.out.print("b ");
+                boy = true;
+            } else {
+                System.out.print("g ");
+                girl = true;
+            }
+            total++;
+        }
+        System.out.println("You made it... and you now have " + total + " children.");
+    }
 }
