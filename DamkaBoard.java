c99fe95dd641c188890d033dcec3ffed6e86c66e
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-31 11:24:08 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,13 +1,12 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 	int n = Integer.parseInt(args[0]);
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ((i + j) % 2 == 0) {
-                    System.out.print("*  ");
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+                    System.out.print("* ");
                 } else {
-                    System.out.print("   ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
